870eefa94ff2376566ed520fe7bbc8f63c4c480f
Christopher Snowhill <kode54@gmail.com>
2018-12-30 14:11:04 -0800

    Build on multiple threads for faster building

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-@cl /O2 /MT /EHsc /D_USING_V110_SDK_71_ /Dstrcasecmp=_stricmp /Dstrncasecmp=_strnicmp  2mid.cpp midi_processing\*.cpp /link /subsystem:console,5.01
+@cl /O2 /MT /EHsc /MP /D_USING_V110_SDK_71_ /Dstrcasecmp=_stricmp /Dstrncasecmp=_strnicmp  2mid.cpp midi_processing\*.cpp /link /subsystem:console,5.01
