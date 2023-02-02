5e69ab75711ec44960c8541bdaa7e54754d737a7
Christopher Snowhill <kode54@gmail.com>
2023-02-02 03:15:47 -0800

    Removed json text preparation step
    Removed that step from the build scripts, since we now use a
    binary json object, which is already prepared into `8820.h`.
    
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- build-osx.sh
+++ build-osx.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-cat json_header 8820.json json_footer > 8820json.h
 
 clang++ -Os -std=c++11 2mid.cpp midi_processing/*.cpp -o 2mid
--- build.bat
+++ build.bat
@@ -1,2 +1,1 @@
-@copy /y json_header + 8820.json + json_footer 8820json.h
 @cl /O2 /MT /EHsc /MP /D_USING_V110_SDK_71_ /Dstrcasecmp=_stricmp /Dstrncasecmp=_strnicmp  2mid.cpp midi_processing\*.cpp /link /subsystem:console,5.01
