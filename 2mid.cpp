167b553a6b580122375d483a746616e56c48413f
Christopher Snowhill <kode54@gmail.com>
2024-01-28 23:54:02 -0800

    Rename a single function (type1 -> form_1)
    Also update midi_processing library
    
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- 2mid.cpp
+++ 2mid.cpp
@@ -155,7 +155,7 @@ int main(int argc, char ** argv)
 
 				midi_processor::process_file( buffer, in_extension, container );
 
-				if ( arg_1 ) container.promote_to_type1();
+				if ( arg_1 ) container.promote_to_form_1();
  
 				if ( arg_h ) container.apply_hackfix( arg_h_value );
 
